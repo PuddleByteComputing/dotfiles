b8a36a6f12e3383007338c4068ed338fbdbbdf59
Ed Slocomb <edslocomb@gmail.com>
2018-09-15 18:28:35 -0700

    clean up emacs init, get flow+flycheck working

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,41 +1,46 @@
-(defvar root-dir "~/.emacs.d/")
-(defun load-lib (name) (load (concat root-dir name ".el")))
-(defun load-lib-dir (path) (add-to-list 'load-path (concat root-dir path)))
-(load-lib-dir ".")
-(load-lib-dir "./lib")
+;;; Init --- startup starts here
+;;; Commentary:
+;;;   If you want to add something specific to a particular mode, put it in modes.el, not here.
+;;; Code:
 
-;;;; package management
+;;;; Set paths
+(defvar root-dir "~/.emacs.d/")
+(add-to-list 'load-path (concat root-dir "lisp"))
+
+;;;; Package management
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
-
 (when (not package-archive-contents)
   (package-refresh-contents))
-
 ;; simple way to synchronize a bunch of packages across installs.
 ;;   (there are more sophisticated ways to do this, like pallet
 ;;    or el-get, but this seems to work fine for now)
-(defvar my-packages '(ag
+(defvar my-packages '(add-node-modules-path
+                      ag
                       applescript-mode
-                      ;; auto-complete
+                      auto-complete
                       coffee-mode
                       css-mode
-                      dropdown-list
+                      ;; dropdown-list
                       expand-region
                       flycheck
+                      flycheck-flow
+                      graphql-mode
                       highlight-indentation
                       js2-mode
                       json-mode
+                      magit
                       markdown-mode
                       multiple-cursors
+                      prettier-js
                       projectile
                       rainbow-mode
                       robe
                       rvm
-                      scss-mode
                       slim-mode
                       string-inflection
                       web-mode
@@ -53,12 +58,22 @@
 (setq initial-major-mode (quote text-mode))
 (setq initial-scratch-message nil)
 (setq-default indent-tabs-mode nil)
+;; Save squiggle files somewhere out of the way
+(defconst emacs-tmp-dir "~/.emacs_saves/")
+(make-directory emacs-tmp-dir t)
+(setq backup-directory-alist
+      `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms
+      `((".*" ,emacs-tmp-dir t)))
 
 ;;;; Server Mode
 (server-mode)
 
 ;;;; Hooks
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(add-hook 'before-save-hook 'whitespace-cleanup)
+
+;;;; Don't mess with our Tmux config
+(global-unset-key (kbd "C-]"))
 
 ;;;; Shortcuts
 (global-set-key (kbd "M-g") 'goto-line)
@@ -68,66 +83,36 @@
 (global-set-key (kbd "M-n") (lambda () (interactive) (scroll-up 1)))
 (global-set-key (kbd "M-#") 'linum-mode)
 (global-set-key (kbd "M-+") 'ido-mode)
+(global-set-key (kbd "M-s")     'fixup-whitespace) ; best function ever
+(savehist-mode 1) ;; save your minibuffer history
+(global-set-key (kbd "C-c r") 'revert-buffer) ;; not quite the same as c-x c-v RET, but close
+(put 'upcase-region 'disabled nil)
 
-;; Save squiggle files somewhere out of the way
-(defconst emacs-tmp-dir "~/.emacs_saves/")
-(make-directory emacs-tmp-dir t)
-(setq backup-directory-alist
-      `((".*" . ,emacs-tmp-dir)))
-(setq auto-save-file-name-transforms
-      `((".*" ,emacs-tmp-dir t)))
 
-(load-lib "modes")
+
+
+
+;;;; DO NOT ADD ANYTHING BELOW THIS LINE
+
+
+
+
+;; mode-specific initialization
+(load (concat root-dir "modes.el"))
+
+;; stuff added by Custom
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2)
- '(custom-safe-themes
-   (quote
-    ("235dc2dd925f492667232ead701c450d5c6fce978d5676e54ef9ca6dd37f6ceb" "71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" "8fed5e4b89cf69107d524c4b91b4a4c35bcf1b3563d5f306608f0c48f580fdf8" "90edd91338ebfdfcd52ecd4025f1c7f731aced4c9c49ed28cfbebb3a3654840b" "f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" "8ac31e1bc1920b33d478dfafb0b45989a00ede15a2388ea16093e7d0988c48d0" "60f04e478dedc16397353fb9f33f0d895ea3dab4f581307fbf0aa2f07e658a40" "16248150e4336572ff4aa21321015d37c3744a9eb243fbd1e934b594ff9cf394" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
- '(package-selected-packages
-   (quote
-    (projectile yaml-mode web-mode string-inflection slim-mode scss-mode rvm robe rainbow-mode multiple-cursors markdown-mode magit json-mode js2-mode highlight-indentation flycheck expand-region ensime dropdown-list coffee-mode auto-complete applescript-mode ag)))
- '(safe-local-variable-values (quote ((encoding . utf-8)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
 
-;; stuff from zenspider
-(require 'bs)
-(global-set-key (kbd "C-x C-b") 'bs-show) ; better buffer listings
-(global-set-key (kbd "M-s")     'fixup-whitespace) ; best function ever
-
-(require 'ruby-mode)
-(require 'rcodetools)
-(define-key ruby-mode-map (kbd "C-c C-z") 'xmp)
-
-(savehist-mode 1)
-
-(global-set-key (kbd "C-c r") 'revert-buffer)
-
-;; magit
-(global-set-key (kbd "C-x g") 'magit-status)
-
-(eval-after-load "compile"
-  '(progn
-     ;; For eslint:
-     (add-to-list 'compilation-error-regexp-alist
-		  '("^\\(/[^\":\n]+\\)\n *\\([0-9]+\\):[0-9]+ +\\(error\\|warning\\) +" 1 2))))
-
-;; added this to enable debugging in rspec buffers.
-(add-hook 'after-init-hook 'inf-ruby-switch-setup)
-
-;; DO THIS LAST
-
+;; DO THIS LAST - load local user config
 ;; load .emacs_local.el, if present
 (if (file-exists-p (setq local-init-file "~/.emacs_local.el"))
     (load local-init-file))
 
-;; DO NOT ADD ANYTHING BELOW THIS
+(provide 'init)
+;;; init.el ends here
--- emacs.d/modes.el
+++ emacs.d/modes.el
@@ -1,14 +1,34 @@
-;;;; Modes
+;;; Modes --- mode-specific initialization and configuration
+;;; Commentary:
+;;;   Try to keep things in here alphabetized by mode
+;;; Code:
 
 ;; ag (ag > ack)
 (global-set-key (kbd "C-c k") 'ag)
 
-;; autocomplete
-;; (require 'auto-complete-config)
-;; (ac-config-default)
-;; ;; (setq ac-ignore-case nil)
-;; ;; (add-to-list 'ac-modes 'enh-ruby-mode)
-;; (add-to-list 'ac-modes 'web-mode)
+;; auto-complete
+(ac-config-default)
+;; (setq ac-ignore-case nil)
+;; (add-to-list 'ac-modes 'enh-ruby-mode)
+(add-to-list 'ac-modes 'web-mode)
+
+;; buffer selection
+(require 'bs)
+(global-set-key (kbd "C-x C-b") 'bs-show) ; better buffer listings
+
+;; cnf files (e.g. mysql config files)
+(add-to-list 'auto-mode-alist '("\\.cnf$" . conf-mode))
+
+;; coffee
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+(custom-set-variables '(coffee-tab-width 2))
+
+;; eslint
+(eval-after-load "compile"
+  '(progn
+     (add-to-list 'compilation-error-regexp-alist
+                  '("^\\(/[^\":\n]+\\)\n *\\([0-9]+\\):[0-9]+ +\\(error\\|warning\\) +" 1 2))))
 
 ;; expand-region
 (require 'expand-region)
@@ -16,86 +36,46 @@
 
 ;; flycheck
 (require 'flycheck)
+(require 'flycheck-flow)
 (add-hook 'after-init-hook #'global-flycheck-mode)
-(add-hook 'scss-mode-hook 'flycheck-mode);; disable jshint
+(add-hook 'js-mode-hook (lambda () (flycheck-mode t)))
+(add-hook 'scss-mode-hook 'flycheck-mode)
+;; disable jshint
 (setq-default flycheck-disabled-checkers
               (append flycheck-disabled-checkers
                       '(javascript-jshint)))
-;; use eslint with web-mode for jsx files
+;; pick up project/local install of eslint
+(eval-after-load 'web-mode
+  '(add-hook 'web-mode-hook #'add-node-modules-path))
+;; use flow with web-mode for js/jsx files
+(flycheck-add-mode 'javascript-flow 'web-mode)
+;; use eslint after flow
 (flycheck-add-mode 'javascript-eslint 'web-mode)
-;; use eslint from project ./node_modules or ./bin when available
-;; see: http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
-(defun my/use-eslint-from-local-project ()
- (let* ((root (condition-case nil (projectile-project-root) (error nil)))
-        (eslint-node (expand-file-name "node_modules/eslint/bin/eslint.js" root))
-        (eslint-binstub (expand-file-name "bin/eslint" root)))
-   (when (file-executable-p eslint-node)
-     (setq-local flycheck-javascript-eslint-executable eslint-node))
-   (when (file-executable-p eslint-binstub)
-     (setq-local flycheck-javascript-eslint-executable eslint-binstub))))
-(add-hook 'flycheck-mode-hook #'my/use-eslint-from-local-project)
-
-
-;; highlight-indentation commented out until fixed with newer web-mode:
-;; https://github.com/antonj/Highlight-Indentation-for-Emacs/pull/27
-;;
-;; highlight-indentation
-;; (require 'highlight-indentation)
-;; (add-hook 'ruby-mode-hook
-;;          (lambda () (highlight-indentation-current-column-mode)))
-;; (add-hook 'coffee-mode-hook
-;;          (lambda () (highlight-indentation-current-column-mode)))
-;; (add-hook 'js-mode-hook
-;;          (lambda () (highlight-indentation-current-column-mode)))
-;; (add-hook 'js2-mode-hook
-;;          (lambda () (highlight-indentation-current-column-mode)))
-;; (add-hook 'web-mode-hook
-;;          (lambda () (highlight-indentation-current-column-mode)))
-
-;; io -- Note: as of 9/2013 there was no io-mode on marmalade.
-(add-to-list 'load-path "~/.emacs.d/io-mode")
-(autoload 'io-mode "io-mode" "Mode for editing Io files" t)
-(add-to-list 'auto-mode-alist '("\\.io$" . io-mode))
-
-;; prettier
-(require 'prettier-js)
-(add-hook 'js2-mode-hook 'prettier-js-mode)
-(add-hook 'web-mode-hook 'prettier-js-mode)
+(flycheck-add-next-checker 'javascript-flow 'javascript-eslint)
 
 ;; js-mode, js2-mode
 (setq js-indent-level 2)
 
-;; inline error checking via flycheck
-(require 'flycheck)
-(add-hook 'js-mode-hook
-          (lambda () (flycheck-mode t)))
-
-;; jsx syntax highlighting - see web-mode below
-;; jsx inline error checking
-;; (flycheck-define-checker jsxhint-checker
-;;   "A JSX syntax and style checker based on JSXHint."
-;;   :command ("jsxhint"
-;;             (config-file "--config" flycheck-jshintrc)
-;;             source)
-;;   :error-patterns
-;;   ((error line-start (1+ nonl) ": line " line ", col " column ", " (message) line-end))
-;;   :modes (web-mode))
-;; (add-hook 'web-mode-hook
-;;           (lambda ()
-;;             (when (equal web-mode-content-type "jsx")
-;;               ;; enable flycheck
-;;               (flycheck-select-checker 'jsxhint-checker)
-;;               (flycheck-mode))))
+;; magit
+(global-set-key (kbd "C-x g") 'magit-status)
 
 ;; multiple-cursors
 (require 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 
-;; mysql cnf files
-(add-to-list 'auto-mode-alist '("\\.cnf$" . conf-mode))
-
 ;; markdown mode for *.md files.
 (add-to-list 'auto-mode-alist '("\.md$" . markdown-mode))
+
+;; prettier-js - format js/jsx on save
+(require 'prettier-js)
+(defun enable-minor-mode (my-pair)
+  "Enable minor mode if filename match the regexp.  MY-PAIR is a cons cell (regexp . minor-mode)."
+  (if (buffer-file-name)
+      (if (string-match (car my-pair) buffer-file-name)
+      (funcall (cdr my-pair)))))
+(add-hook 'web-mode-hook #'(lambda ()
+                             (enable-minor-mode
+                              '("\\.jsx?\\'" . prettier-js-mode))))
 
 ;; projectile
 (require 'projectile)
@@ -105,11 +85,17 @@
 (setq rainbow-html-colors 'auto)
 
 ;; robe
-;;  prevent "WARNING: terminal is not fully functional" when inf-ruby starts
-(setenv "PAGER" "cat")
+;;   first tell inf-ruby to always use the 'development' Rails environment, instead of prompting.
+(setq inf-ruby-console-environment "development")
 (add-hook 'ruby-mode-hook 'robe-mode)
+(defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
+  (rvm-activate-corresponding-ruby))
 
 ;; ruby
+
+(require 'ruby-mode)
+(define-key ruby-mode-map (kbd "C-c C-z") 'xmp)
+(setq ruby-align-chained-calls t)
 (autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
 (setq auto-mode-alist (append '(("\\.rb$"   . ruby-mode)
                                 ("\\.rake$"   . ruby-mode)
@@ -121,6 +107,15 @@
                                 ("Capfile"   . ruby-mode)
                                 ("Gemfile"   . ruby-mode)
                                 ) auto-mode-alist))
+;; rvm
+(require 'rvm)
+(rvm-autodetect-ruby)
+
+;; css
+(setq cssm-indent-level 2)
+(setq cssm-newline-before-closing-bracket t)
+(setq cssm-indent-function #'cssm-c-style-indenter)
+(setq css-indent-offset 2)
 
 ;; sass - don't compile on save
 (add-hook 'scss-mode-hook
@@ -128,37 +123,61 @@
              (setq scss-compile-at-save nil)
              (setq css-indent-offset 2)))
 
-(add-hook 'scss-mode-hook 'rainbow-mode)
+;; string-inflection - cycle between snake case, camel case, etc.
+(require 'string-inflection)
+(global-set-key (kbd "C-c i") 'string-inflection-cycle)
+(global-set-key (kbd "C-c c") 'string-inflection-camelcase)        ;; Force to CamelCase
+(global-set-key (kbd "C-c l") 'string-inflection-lower-camelcase)  ;; Force to lowerCamelCase
+(global-set-key (kbd "C-c j") 'string-inflection-java-style-cycle) ;; Cycle through Java styles
 
 ;; web-mode
 (add-to-list 'auto-mode-alist '("\\html.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\text.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js.erb$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?.erb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.es6$" . web-mode))
-(setq web-mode-markup-indent-offset 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
-(setq web-mode-attr-indent-offset 2)
+(add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
 
-;; tell web-mode to treat .es6 files as jsx
-(setq web-mode-content-types-alist '(("jsx"  . "\\.es6\\'")))
-
-;; use web-mode for jsx - https://truongtx.me/2014/03/10/emacs-setup-jsx-mode-and-jsx-syntax-checking/
+;; use web-mode for jsx - https://truongtx.me/2014/03/10/emacs-setup-jsx-mode-and-jsx-syntax-checking
 (defadvice web-mode-highlight-part (around tweak-jsx activate)
   (if (equal web-mode-content-type "jsx")
       (let ((web-mode-enable-part-face nil))
         ad-do-it)
     ad-do-it))
 
+;; highlight jsx in .js files
+(setq web-mode-content-types-alist
+  '(("jsx" . "\\.js[x]?\\'")))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-attr-indent-offset 2)
+  (setq web-mode-ac-sources-alist
+        '(("css" . (ac-source-words-in-buffer ac-source-css-property))
+          ("html" . (ac-source-words-in-buffer ac-source-abbrev))
+          ("jsx" . (ac-source-words-in-buffer ac-source-words-in-same-mode-buffers))))
+  (when (equal web-mode-content-type "html")
+    (setq flycheck-disabled-checkers
+          (append flycheck-disabled-checkers
+                  '(javascript-eslint)
+                  '(javascript-flow))))
+  (when (equal web-mode-content-type "jsx")
+    (flycheck-select-checker 'javascript-flow)
+    (setq flycheck-disabled-checkers
+          (append flycheck-disabled-checkers
+                  '(json-jsonlist)))))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
 ;; Yasnippet
 (require 'yasnippet)
 ;; use the nice dropdown-list widget, instead of selecting snippets in the minibuffer
-(require 'dropdown-list)
-(setq yas-prompt-functions '(yas-dropdown-prompt
-                             yas-ido-prompt
-                             yas-completing-prompt))
+;; (require 'dropdown-list)
+;; (setq yas-prompt-functions '(yas-dropdown-prompt
+;;                             yas-ido-prompt
+;;                             yas-completing-prompt))
+
 ;; pick up custom snippets in dicksonlabs/snippets
 (add-to-list 'yas-snippet-dirs
              (concat root-dir "dicksonlabs/snippets/") t)
@@ -167,17 +186,10 @@
           #'(lambda () (yas-activate-extra-mode 'html-mode)))
 ;; do this to make the web-mode-hook stick
 (yas-global-mode 1)
-
-;; rvm
-(require 'rvm)
-(rvm-autodetect-ruby) ;;
-
-;; Cycle between snake case, camel case, etc.
-(require 'string-inflection)
-(global-set-key (kbd "C-c i") 'string-inflection-cycle)
-(global-set-key (kbd "C-c c") 'string-inflection-camelcase)        ;; Force to CamelCase
-(global-set-key (kbd "C-c l") 'string-inflection-lower-camelcase)  ;; Force to lowerCamelCase
-(global-set-key (kbd "C-c j") 'string-inflection-java-style-cycle) ;; Cycle through Java styles
+;; remap yasnippet expansion key to ctrl-tab, to prevent interference with auto-complete package
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
 
 
 (provide 'modes)
